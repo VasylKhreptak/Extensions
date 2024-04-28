cea19f740a71b112432fb2b01f4b2a1808d2f4d5
DESKTOP-5QT8APO\Vasyl Khreptak <VasylKhreptak12@gmail.com>
2024-04-28 21:13:11 +0300

    feat: added GameObjectExtensions;

---
+++ Assets/Plugins/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Plugins.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static T TryAddComponent<T>(this GameObject gameObject) where T : Component
+        {
+            if (gameObject.TryGetComponent(out T existingComponent))
+                return existingComponent;
+
+            return gameObject.AddComponent<T>();
+        }
+    }
+}
