93873d3ab63afa2cf2d7c19c607a9b42c90fc080
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2024-09-28 13:37:20 +0300

    Revert "feat: added string extensions;"
    This reverts commit 6b77a711d677b87fd28e9adce0533f6fe268bc16.

--- Assets/Plugins/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Plugins.Extensions
-{
-    public static class StringExtensions
-    {
-        public static string WithColor(this string text, Color color) => $"<color={color}>{text}</color>";
-    }
-}
