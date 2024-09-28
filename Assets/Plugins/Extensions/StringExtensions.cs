59f24318d38bc8892286a5a03b0bbd2ea53729f8
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2024-09-28 13:16:04 +0300

    Revert "feat: added string extensions;"
    This reverts commit 16eba31561e5826a560233dc0772daf10feb344d.

--- Assets/Plugins/Extensions/StringExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Plugins.Extensions
-{
-    public static class StringExtensions
-    {
-        public static string WithColor(this string text, string color) => $"<color={color}>{text}</color>";
-    }
-}
